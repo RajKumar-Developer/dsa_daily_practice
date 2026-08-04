337b8a4a26c2f8f1f976c44fe12e0dce46373887
Rajkumar-Developer <rajkumarrcse28@gmail.com>
2026-08-04 06:57:33 +0530

    pattern 2

---
+++ patterns/pattern2.java
@@ -0,0 +1,14 @@
+//Pattern-2: Right-Angled Triangle Pattern
+public class pattern2 {
+    public static void rightTriangle(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        rightTriangle(5);
+    }
+}
