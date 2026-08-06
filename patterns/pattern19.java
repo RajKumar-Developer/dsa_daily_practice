29546b920f7518b2079833aaeaaaf3d27a1a47e2
Rajkumar-Developer <rajkumarrcse28@gmail.com>
2026-08-07 00:25:50 +0530

    pattern 19 completed

--- patterns/pattern19.java
+++ patterns/pattern19.java
@@ -1,17 +1,50 @@
 // Pattern-19: Symmetric-Void Pattern
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
 public class pattern19 {
     public static void main(String[] args) {
         symmetricVoidPattern(5);
     }
 
     private static void symmetricVoidPattern(int n) {
-        for(int i=0;i<2*n;i++){
+        //upper pattern
+        for(int i=0;i<n;i++){
             //lefttop
             for(int j=0;j<n-i;j++){
                 System.out.print("*");
             }
             //topmidspace
-            // for(int j=0;j<)
+            for(int j=0;j<2*i;j++){
+                System.err.print(" ");
+            }
+            //toprightspace
+            for(int j=0;j<n-i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //lower pattern
+        for(int i=0;i<n;i++){
+            //bottomLeft;
+            for(int j=0;j<i+1;j++){
+                System.out.print("*");
+            }
+            //bottomMid
+            for(int j=0;j<2*(n-i)-2;j++){
+                System.out.print(" ");
+            }
+            //bottomRight
+            for(int j=0;j<i+1;j++){
+                System.out.print('*');
+            }
             System.out.println();
         }
     }
