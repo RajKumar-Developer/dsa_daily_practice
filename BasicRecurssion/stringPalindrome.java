29f17edeb5d9cfe8c776878bb9d9ae308c29b600
Rajkumar-Developer <rajkumarrcse28@gmail.com>
2026-08-11 23:59:48 +0530

    basicRecurssion/rec pal

---
+++ BasicRecurssion/stringPalindrome.java
@@ -0,0 +1,31 @@
+package BasicRecurssion;
+// Check if the given String is Palindrome or not
+public class stringPalindrome {
+    public static void main(String[] args) {
+        System.out.println(bruteforce("MALAYALAM")?"Palindrome":"Not A Palindrome");
+    }
+    public static boolean recurssionPalindrome(int i,String str){
+        if(i>=str.length()/2){
+            return true;
+        }
+        if(str.charAt(i)!=str.charAt(str.length()-1-i)){
+            return false;
+        }
+    }
+    public static boolean bruteforce(String str){
+        int p1 = 0,p2 = str.length()-1;
+        while(p1<p2){
+            if(!Character.isLetterOrDigit(str.charAt(p1))){
+                p1++;
+            }else if(!Character.isLetterOrDigit(str.charAt(p2))){
+                p2--;
+            }else if(Character.toLowerCase(str.charAt(p1))!=Character.toLowerCase(str.charAt(p2))){
+                return false;
+            }else{
+                p1++;
+                p2--;
+            }
+        }
+        return true;
+    }
+}
