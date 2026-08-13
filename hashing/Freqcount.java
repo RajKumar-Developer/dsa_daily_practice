001ba2464e2536fe5ea3f29f9315240f98b25950
Rajkumar-Developer <rajkumarrcse28@gmail.com>
2026-08-14 00:16:10 +0530

    hashing/freqOptimized

--- hashing/Freqcount.java
+++ hashing/Freqcount.java
@@ -1,9 +1,23 @@
 package hashing;
+
+import java.util.*;
+
 // Count frequency of each element in the array
 public class Freqcount {
     public static void main(String[] args) {
         int[] arr = {5,10,5,15,10,5,2};
-        countTheFrequencyBrute(arr);
+        // countTheFrequencyBrute(arr);
+        frequencyMap(arr);
+    }
+
+    private static void frequencyMap(int[] arr) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int a:arr){
+            map.put(a,map.getOrDefault(a,0)+1);
+        }
+        for(Map.Entry<Integer,Integer> m:map.entrySet()){
+            System.out.println(m.getKey()+":"+m.getValue());
+        }
     }
 
     private static void countTheFrequencyBrute(int[] arr) {
