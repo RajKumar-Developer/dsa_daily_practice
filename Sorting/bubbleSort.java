f2131f72a3515167e59bdb4e80377294de7a5ed2
Rajkumar-Developer <rajkumarrcse28@gmail.com>
2026-08-15 23:58:19 +0530

    sorting/bubbleopt

--- Sorting/bubbleSort.java
+++ Sorting/bubbleSort.java
@@ -7,7 +7,23 @@ public class bubbleSort {
         int[] arr = {9,4,7,3,5,2,1,6};
         System.out.println(Arrays.toString(bruteBubbleSort(arr)));
     }
-
+    public void bubbleSortopt(int[] arr) {
+        int n = arr.length;
+        for (int i = n - 1; i >= 0; i--) {  //Use two nested loops to iterate over the array
+            boolean didSwap = false;
+            for (int j = 0; j <= i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j + 1];  //Swap arr[j+1] with arr[i]
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                    didSwap = true;
+                }
+            }
+            if (!didSwap) {
+                break;
+            }
+        }
+    }
     private static int[] bruteBubbleSort(int[] arr) {
         int n = arr.length;
         for (int i = n - 1; i >= 0; i--) {   //7 6 5 4 3 2 1 0
